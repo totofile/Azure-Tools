b769472da8a80fc55d8df5ba571473b73249cbfe
totofile <theophilenp@gmail.com>
2024-08-09 15:37:56 +0200

    feat/filter+filesorganization

---
+++ web-secret-cert-expiry/web-secret-cert-expiry/components/body.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, {useState} from 'react';
+import { useMsal } from '@azure/msal-react';
+import { InteractionRequiredAuthError, InteractionType } from '@azure/msal-browser';
+import { Client } from '@microsoft/microsoft-graph-client';
+import { AuthCodeMSALBrowserAuthenticationProvider } from '@microsoft/microsoft-graph-client/authProviders/authCodeMsalBrowser';
+import LoginConfig  from './auth';
+
+interface BodyProps {
+  isAuth2: boolean;
+}
+
+
+const Body: React.FC<BodyProps> = () => {
+  return (
+    <div>
+      {isAuth ? <h1>User is authenticated</h1> : <h1>User is not authenticated</h1>}
+    </div>
+  );
+};
+
+export default Body;
--- web-secret-cert-expiry/web-secret-cert-expiry/components/corps.tsx
+++ web-secret-cert-expiry/web-secret-cert-expiry/components/corps.tsx
@@ -5,24 +5,14 @@ import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
 import { Client } from '@microsoft/microsoft-graph-client';
 import { AuthCodeMSALBrowserAuthenticationProvider } from '@microsoft/microsoft-graph-client/authProviders/authCodeMsalBrowser';
 import LoginConfig from './auth';
+import { formatDate, calculateDaysToExpiry } from './dateUtils';
+import { fetchApplications, fetchSecrets, fetchCertificates } from './applicationService';
 
-// Function to format date
-const formatDate = (dateString: string) => {
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-};
-// Function to calculate days to expiry
-const calculateDaysToExpiry = (dateString: string) => {
-    const expiryDate = new Date(dateString);
-    const today = new Date();
-    const timeDiff = expiryDate.getTime() - today.getTime();
-    return Math.ceil(timeDiff / (1000 * 3600 * 24));
-};
 const Corps: React.FC = () => {
     const [isAuth, setIsAuth] = useState(false);
     const [applications, setApplications] = useState<any[]>([]);
-    const [selectedType, setSelectedType] = useState<string>('secrets');
-    const [daysToExpiry, setDaysToExpiry] = useState<number>(0);
+    const [selectedType, setSelectedType] = useState<string>('all');
+    const [daysToExpiry, setDaysToExpiry] = useState<number>(5000);
     const publicClientAppRef = useRef<PublicClientApplication | null>(null);
 
     useEffect(() => {
@@ -52,35 +42,31 @@ const Corps: React.FC = () => {
             await publicClientAppRef.current?.loginPopup();
             console.log("Login successful");
             setIsAuth(true);
-            fetchApplications(); // Fetch applications after login
+            handleFetchApplications(); // Fetch applications after login
         } catch (error) {
             console.error("Login failed", error);
         }
     };
 
-    const fetchApplications = async () => {
+    const handleFetchApplications = async () => {
         if (!publicClientAppRef.current) return;
-    
+
         const authProvider = new AuthCodeMSALBrowserAuthenticationProvider(publicClientAppRef.current, {
             account: publicClientAppRef.current.getAllAccounts()[0],
             scopes: ["Directory.Read.All"],
             interactionType: InteractionType.Popup,
         });
-    
+
         const client = Client.initWithMiddleware({ authProvider });
-    
+
         try {
-            const response = await client.api('/applications').get();
-            const applications = response.value;
-    
-            // Fetch secrets and certificates in parallel
+            const applications = await fetchApplications(client);
             const [appsWithSecrets, appsWithCertificates] = await Promise.all([
                 fetchSecrets(client, applications),
                 fetchCertificates(client, applications)
             ]);
-    
-            // Merge secrets and certificates into applications
-            const mergedApplications: any[] = applications.map((app: any) => {
+
+            const mergedApplications = applications.map((app: any) => {
                 const appWithSecrets = appsWithSecrets.find((a: any) => a.id === app.id) || {};
                 const appWithCertificates = appsWithCertificates.find((a: any) => a.id === app.id) || {};
                 return {
@@ -89,46 +75,18 @@ const Corps: React.FC = () => {
                     certificates: appWithCertificates.certificates || []
                 };
             });
-    
+
             setApplications(mergedApplications);
         } catch (error) {
             console.error("Fetching applications failed", error);
         }
     };
 
-    const fetchSecrets = async (client: Client, applications: any[]) => {
-        try {
-            const promises = applications.map(async (app) => {
-                const secrets = await client.api(`/applications/${app.id}/passwordCredentials`).get();
-                return { ...app, secrets: secrets.value };
-            });
-            return await Promise.all(promises);
-        } catch (error) {
-            console.error("Fetching secrets failed", error);
-            return [];
-        }
-    };
-    
-    const fetchCertificates = async (client: Client, applications: any[]) => {
-        try {
-            const promises = applications.map(async (app) => {
-                const certificates = await client.api(`/applications/${app.id}/keyCredentials`).get();
-                return { ...app, certificates: certificates.value };
-            });
-            return await Promise.all(promises);
-        } catch (error) {
-            console.error("Fetching certificates failed", error);
-            return [];
-        }
-    };
-    const filteredApplications = applications.map(app => {
+    const filteredApplications = applications.filter(app => app.secrets?.length > 0 || app.certificates?.length > 0).map(app => {
         const filteredSecrets = app.secrets?.filter((secret: any) => calculateDaysToExpiry(secret.endDateTime) <= daysToExpiry) || [];
         const filteredCertificates = app.certificates?.filter((cert: any) => calculateDaysToExpiry(cert.endDateTime) <= daysToExpiry) || [];
-        return {
-            ...app,
-            secrets: filteredSecrets,
-            certificates: filteredCertificates
-        };
+
+        return { ...app, secrets: filteredSecrets, certificates: filteredCertificates };
     });
 
     return (
@@ -139,99 +97,103 @@ const Corps: React.FC = () => {
                     {isAuth ? "Logged In" : "Login"}
                 </button>
             </header>
-            <div className="text-lg mx-20 ">
-                <div className=" flex justify-between items-center bg-cyan-500 text-black text-center rounded p-4 mx-auto mt-10 mb-10" >
+            <div className="text-lg mx-20">
+                <div className="flex justify-between items-center bg-cyan-500 text-black text-center rounded p-4 mx-auto mt-10 mb-10">
                     <h1>Secrets / Certificats</h1>
-                    <button onClick={fetchApplications}>Fetch Applications</button>
+                    <button onClick={handleFetchApplications}>Fetch Applications</button>
                     <select onChange={(e) => setSelectedType(e.target.value)} value={selectedType}>
+                        <option value="all">All</option>
                         <option value="secrets">Secrets</option>
                         <option value="certificates">Certificates</option>
                     </select>
-                    <input type="number" placeholder='Days to expiry' onChange={(e) => setDaysToExpiry(Number(e.target.value))} /></div>
+                    <input type="number" placeholder='Days to expiry : 30' onChange={(e) => setDaysToExpiry(Number(e.target.value))} />
+                </div>
                 <div>
                     {filteredApplications.length > 0 ? (
-                        <table className="w-full text-xl text-left " >
-                            <thead className=' text-sm text-gray-700 uppercase'>
-                                <tr className="">
-                                    <th scope="col" className="border  border-gray-700 px-6 py-3 bg-gray-50 ">Application Name</th>
-                                    <th scope="col" className="border  border-gray-700 px-6 py-3 ">{selectedType === 'secrets' ? 'Secret Display Name' : 'Certificate Display Name'}</th>
+                        <table className="w-full text-xl text-left">
+                            <thead className='text-sm text-gray-700 uppercase'>
+                                <tr>
+                                    <th scope="col" className="border border-gray-700 px-6 py-3 bg-gray-50">Application Name</th>
+                                    <th scope="col" className="border border-gray-700 px-6 py-3">{selectedType === 'all' ? 'Display Name' : selectedType === 'secrets' ? 'Secret Display Name' : 'Certificate Display Name'}</th>
                                     <th scope="col" className="border border-gray-700 px-6 py-3 bg-gray-50">End Date</th>
-                                    <th scope="col" className="border border-gray-700 px-6 py-3 bg-gray-50">Days To Expiry</th>
-
+                                    <th scope="col" className="border border-gray-700 px-6 py-3">Days To Expiry</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {filteredApplications.map((app) => (
                                     <tr key={app.id}>
-                                        <td scope="col" className=" border border-gray-700 py-3 bg-gray-50 text-left  p-5 ">{app.displayName}</td>
+                                        <td scope="col" className="border border-gray-700 py-3 bg-gray-50 text-left p-5">{app.displayName}</td>
                                         <td className="border border-gray-700 p-5">
-                                            {selectedType === 'secrets' ? (
+                                            {selectedType === 'secrets' || selectedType === 'all' ? (
                                                 app.secrets && app.secrets.length > 0 ? (
                                                     app.secrets.map((secret: any) => (
-                                                        <div className=" border-gray-700"key={secret.keyId}>
+                                                        <div className="border-gray-700" key={secret.keyId}>
                                                             <p>{secret.displayName}</p>
                                                         </div>
                                                     ))
                                                 ) : (
-                                                    <p>No secrets found</p>
+                                                    null
                                                 )
-                                            ) : (
+                                            ) : null}
+                                            {selectedType === 'certificates' || selectedType === 'all' ? (
                                                 app.certificates && app.certificates.length > 0 ? (
                                                     app.certificates.map((cert: any) => (
-                                                        <div  className=" border-gray-700  " key={cert.keyId}>
+                                                        <div className="border-gray-700" key={cert.keyId}>
                                                             <p>{cert.displayName}</p>
                                                         </div>
                                                     ))
                                                 ) : (
-                                                    <p>No certificates found</p>
+                                                    null
                                                 )
-                                            )}
+                                            ) : null}
                                         </td>
-                                        <td scope="col" className="border border-gray-700 py-3 bg-gray-50 p-5" >
-                                            {selectedType === 'secrets' ? (
+                                        <td scope="col" className="border border-gray-700 py-3 bg-gray-50 p-5">
+                                            {selectedType === 'secrets' || selectedType === 'all' ? (
                                                 app.secrets && app.secrets.length > 0 ? (
                                                     app.secrets.map((secret: any) => (
-                                                        <div className=" border-gray-700" key={secret.keyId}>
+                                                        <div className="border-gray-700" key={secret.keyId}>
                                                             <p>{formatDate(secret.endDateTime)}</p>
                                                         </div>
                                                     ))
                                                 ) : (
-                                                    <p>No secrets found</p>
+                                                    null
                                                 )
-                                            ) : (
+                                            ) : null}
+                                            {selectedType === 'certificates' || selectedType === 'all' ? (
                                                 app.certificates && app.certificates.length > 0 ? (
                                                     app.certificates.map((cert: any) => (
-                                                        <div key={cert.keyId}>
+                                                        <div className="border-gray-700" key={cert.keyId}>
                                                             <p>{formatDate(cert.endDateTime)}</p>
                                                         </div>
                                                     ))
                                                 ) : (
-                                                    <p>No certificates found</p>
+                                                    null
                                                 )
-                                            )}
+                                            ) : null}
                                         </td>
-                                        <td scope="col" className="border border-gray-700 py-3 bg-gray-50 p-5" >
-                                            {selectedType === 'secrets' ? (
+                                        <td scope="col" className="border border-gray-700 py-3 p-5">
+                                            {selectedType === 'secrets' || selectedType === 'all' ? (
                                                 app.secrets && app.secrets.length > 0 ? (
                                                     app.secrets.map((secret: any) => (
-                                                        <div className=" border-gray-700" key={secret.keyId}>
+                                                        <div className="border-gray-700" key={secret.keyId}>
                                                             <p>{calculateDaysToExpiry(secret.endDateTime)}</p>
                                                         </div>
                                                     ))
                                                 ) : (
-                                                    <p>No secrets found</p>
+                                                    null
                                                 )
-                                            ) : (
+                                            ) : null}
+                                            {selectedType === 'certificates' || selectedType === 'all' ? (
                                                 app.certificates && app.certificates.length > 0 ? (
                                                     app.certificates.map((cert: any) => (
-                                                        <div key={cert.keyId}>
+                                                        <div className="border-gray-700" key={cert.keyId}>
                                                             <p>{calculateDaysToExpiry(cert.endDateTime)}</p>
                                                         </div>
                                                     ))
                                                 ) : (
-                                                    <p>No certificates found</p>
+                                                    null
                                                 )
-                                            )}
+                                            ) : null}
                                         </td>
                                     </tr>
                                 ))}
@@ -245,4 +207,5 @@ const Corps: React.FC = () => {
         </div>
     );
 };
+
 export default Corps;
